d358240b1a977880860a9e53cf7367243950fda3
Sourav Kumar Agarwal <aggarwalsaurav948@gmail.com>
2024-11-28 03:33:18 +0530

    update--6

--- frontend/app/components/profile/Settings.jsx
+++ frontend/app/components/profile/Settings.jsx
@@ -144,7 +144,7 @@ const SettingsModal = ({ isOpen, setIsOpen, user, setUserLogout, refetch }) => {
       const fullVoice = availableVoices.find(
         (voice) => voice.name === selectedVoice
       );
-      toast.success(fullVoice.lang);
+      toast.success(fullVoice.lang, fullVoice.voiceURI);
       utterance.voice = fullVoice;
       speechSynthesis.speak(utterance);
     }
